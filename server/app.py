7c5d16f3770611ef0d96fc613817a782b18f6618
tanvir <tanviralam451@gmail.com>
2025-11-22 20:03:50 +0600

    API import and mount in main application

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from .api_book_management import app as book_management # relative import
 from .api_user_management import app as user_management # relative import
-# from .api_borrow_return import app as borrow_return # relative import
+from .api_borrow_return import app as borrow_return # relative import
 
 app = FastAPI(
     title="Library Management System",
@@ -12,6 +12,7 @@ app = FastAPI(
 
 app.mount("/book", book_management)
 app.mount("/user", user_management)
+app.mount("/borrow", borrow_return)
 
 @app.get("/", response_model=dict, status_code=200)
 async def root():
